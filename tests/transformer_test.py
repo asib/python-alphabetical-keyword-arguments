3a2cb30aa24db57481f546912a76c2a1b40865d2
Jacob Fenton <jacob+github@dfenton.xyz>
2023-03-22 17:47:23 +0200

    More test cases.

--- tests/transformer_test.py
+++ tests/transformer_test.py
@@ -1,5 +1,6 @@
 import libcst as cst
 import pytest
+from precisely import assert_that, equal_to
 
 from python_alphabetical_keyword_arguments.transformer import (
     FunctionParametersTransformer,
@@ -28,11 +29,33 @@ def _result(code: str) -> str:
             id="single positional function parameter",
         ),
         pytest.param(
+            """def f(*, x): pass""",
+            """def f(*, x): pass""",
+            id="single keyword-only function parameter",
+        ),
+        pytest.param(
             """def f(x, *, y): pass""",
             """def f(x, *, y): pass""",
             id="single positional, single keyword-only function parameter",
         ),
+        pytest.param(
+            """def f(*, x, y): pass""",
+            """def f(*, x, y): pass""",
+            id="correctly sorted keyword-only parameters are left alone",
+        ),
+        pytest.param(
+            """def f(*, y, x): pass""",
+            """def f(*, x, y, ): pass""",
+            id="two incorrectly sorted keyword-only parameters are sorted",
+        ),
+        pytest.param(
+            """def f(*, e, a, c, d, b): pass""",
+            """def f(*, a, b, c, d, e, ): pass""",
+            id="five incorrectly sorted keyword-only parameters are sorted",
+        ),
     ),
 )
 def test_all(input_code: str, output_code: str):
-    pass
+    result = _result(input_code)
+
+    assert_that(result, equal_to(output_code))
